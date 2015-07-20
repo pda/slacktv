6d0ef08d8b97be06e568446516d7b446b5f5db2b
Paul Annesley <paul@annesley.cc>
2015-07-19 21:36:42 -0700

    Post replies to greetings and URLs.

--- slacktv.go
+++ slacktv.go
@@ -3,6 +3,7 @@ package slacktv
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"regexp"
 	"strings"
 
@@ -47,12 +48,27 @@ func handleMessage(rtm *slack.RTMSession, ev slack.Event) {
 
 	user := rtm.User(ev["user"].(string))
 	text := ev["text"].(string)
+	chanId := ev["channel"].(string)
 
 	if isMention(text, rtm.Self.Id) {
-		if url, ok := urlFromMessageText(text); ok {
-			dbg("URL from %s: %s", user.Name, url)
-			err := open(url)
+		if isGreeting(text) {
+			err := chatPostMessage(chanId, fmt.Sprintf("hello <@%s>!", user.Id))
 			if err != nil {
+				log.Print(err)
+			}
+		}
+
+		if url, ok := urlFromMessageText(text); ok {
+			err := chatPostMessage(
+				chanId,
+				fmt.Sprintf("<@%s> changing the channel to %s", user.Id, url),
+			)
+			if err != nil {
+				log.Print(err)
+			}
+
+			dbg("URL from %s: %s", user.Name, url)
+			if err := open(url); err != nil {
 				log.Print(err)
 			}
 		}
@@ -74,4 +90,27 @@ func urlFromMessageText(text string) (string, bool) {
 func isGreeting(text string) bool {
 	stripped := tagRegexp.ReplaceAllString(text, "")
 	return strings.Contains(stripped, "hello")
+}
+
+func chatPostMessage(chanId, text string) (err error) {
+	args := url.Values{
+		"channel":      {chanId},
+		"text":         {text},
+		"as_user":      {"true"},
+		"unfurl_links": {"false"},
+	}
+	dbg("chat.postMessage: %#v", args)
+	call, err := slack.Method("chat.postMessage", args)
+	if err != nil {
+		log.Print(err)
+	}
+	var resp slack.ChatPostMessageResponse
+	err = call.Exec(&resp)
+	if err != nil {
+		log.Print(err)
+	}
+	if !resp.Ok {
+		log.Printf("chatPostMessage: %#v\n", resp)
+	}
+	return
 }
