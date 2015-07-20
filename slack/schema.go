d38ce9a6c743cb610e91d42b68c62d04c4649983
Paul Annesley <paul@annesley.cc>
2015-07-19 21:35:09 -0700

    slack/schema.go: ChatPostMessageResponse

--- slack/schema.go
+++ slack/schema.go
@@ -10,13 +10,22 @@ type ChannelResponse struct {
 	Channel ChannelResource
 }
 
+type ChatPostMessageResponse struct {
+	Response
+	Channel string
+	Message MessageResource
+}
+
+type MessageResource struct {
+}
+
 type Response struct {
-	Ok bool
+	Ok    bool
+	Error string
 }
 
 type RTMStartResponse struct {
-	Ok    bool
-	Error string
+	Response
 	Url   string
 	Self  *SelfResource
 	Team  *TeamResource
